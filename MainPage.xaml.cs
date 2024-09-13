b99076b6d31cae178b62a77835d8f7ca9bef843b
Henrique Dias <henrique.d.souza1994@gmail.com>
2024-09-13 19:43:57 -0300

    primeiro commit do projeto

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void RollButton_Clicked(object sender, EventArgs e)
+    {
+        int numeroSelecionado = Convert.ToInt32(SidesPicker.SelectedItem.ToString());
+        int numeroSorteado = new Random().Next(numeroSelecionado) + 1;
+        ResultLabel.Text = numeroSorteado.ToString();
+
+        
+        
+
+    }
+
+
+
+
+
+}
+
